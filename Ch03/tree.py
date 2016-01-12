98e75bd10c5e43c231e89109f2f7c7556d7ac6d9
Shitaibin <james.shi.ch@gmail.com>
2016-01-12 11:44:43 +0800

    frame of decision tree and its test

--- Ch03/tree.py
+++ Ch03/tree.py
@@ -3,3 +3,9 @@ Created on Jan 7, 2016
 Decision Tree Source Code
 @author: Shitaibin
 """
+
+
+class DecisionTree():
+    # TODO: make it happen
+    pass
+
--- Ch03/tree_test.py
+++ Ch03/tree_test.py
@@ -3,3 +3,19 @@ Created on Jan 7, 2016
 Decision Tree Unittest Source Code for tree.py.
 @author: Shitaibin
 """
+
+
+from tree import DecisionTree
+import unittest
+
+
+class DecisionTreeTestCase(unittest.TestCase):
+    """Unittest for tree.DecsionTree
+    """
+    # TODO: make a complete unittest
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
